183a99f289f228fd70f3205d10a9ea083c656c05
himanshi403 <124039917+himanshi403@users.noreply.github.com>
2026-06-30 15:32:09 +0530

    Add Constructor demonstration program
    This Java program demonstrates the use of a constructor that is automatically called when an object is created.

---
+++ 01-Core-Java/Programs/Constructor.java
@@ -0,0 +1,20 @@
+/*
+* Topic:Constructor
+* Description: Demonstrates how constructor is automatically called
+* when object is created
+* Expected Output:
+* Constructor called
+  */
+
+public class Constructor{
+
+  Constructor(){
+    System.out.println("Constructor Called");
+  }
+
+  public static void main(String[] args){
+
+    Constructor obj=new Constructor();
+ }
+}
+  

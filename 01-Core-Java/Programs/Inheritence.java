fb1b68c29078ba4feb01885342ec686e3864833a
himanshi <himanshi403.be22@chitkara.edu.in>
2026-07-05 16:28:42 +0530

    Added Inheritence code

---
+++ 01-Core-Java/Programs/Inheritence.java
@@ -0,0 +1,22 @@
+/*
+Topic: Inheritance
+demonstrating single inheritence in java
+*/
+
+class Animal{
+    void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog barks");
+    }
+}
+public class Inheritence {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.bark();
+    }
+}
